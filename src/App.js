6ab7568b9cdeade3835ada14e78bf5d7bc98b5f1
Clay Palumbo <cpalumbo@firstam.com>
2022-06-17 23:17:14 -0400

    add image

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import React from 'react';
 import Countdown from 'react-countdown';
 
-const Completionist = () => <span>Holy shit thats a lot of work you've done!</span>;
+const Completionist = () => <span>Holy shit man you actually did it!</span>;
 const time = new Date(1775088000000);
+const test = Date.now() + 10000;
 
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
   if (completed) {
@@ -23,6 +24,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="https://i.imgur.com/krIX3Sn.jpg" height={300} alt="Dad"/>
         <div className='container'>
         <div className='title'>Pat's Retirement Countdown</div>
       <Countdown date={time} renderer={renderer}/>
